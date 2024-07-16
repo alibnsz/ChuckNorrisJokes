be1f481796ecc5c392279b4b6855006d7bc25f45
Mehmet Ali Bunsuz <m.bunsuz1@gmail.com>
2024-07-16 17:21:44 +0300

    Initial Commit

---
+++ ChuckNorrisJokes/ChuckNorrisJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisJokesApp.swift
+//  ChuckNorrisJokes
+//
+//  Created by Mehmet Ali Bunsuz on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
